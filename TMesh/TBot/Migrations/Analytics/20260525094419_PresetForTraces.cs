b3927bdea48ac98cfab56343c9794ecdb6fbdcf8
Alexander Shakhov <samfromlv@gmail.com>
2026-05-25 12:44:34 +0300

    Added preset migration for analytics

---
+++ TMesh/TBot/Migrations/Analytics/20260525094419_PresetForTraces.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TBot.Migrations.Analytics
+{
+    /// <inheritdoc />
+    public partial class PresetForTraces : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PresetName",
+                table: "TracePairDevices",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PresetName",
+                table: "TracePairDevices");
+        }
+    }
+}
